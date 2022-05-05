bb5d6293a999d213ab7612fa8226d4c7d498dcea
Kevin Roney <ktroney.pdx@gmail.com>
2022-05-05 11:40:32 -0700

    finished layout on App.js and rfc files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import Movie from './Movie/Movie';
+import MovieForm from './Movie/Movie';
+import MovieList from './MovieList/MovieList';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to MovieList!</h1>
-      
+      <section className='upper-section'>
+        <MovieForm />
+        <Movie />
+      </section>
+      <section className='middle-section'>
+        <label>Filter
+          <input className='filter-input'/>
+        </label>
+      </section>
+      <section className='lower-section'>
+        <MovieList />
+      </section>
     </div>
   );
 }
